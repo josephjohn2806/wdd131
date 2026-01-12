0cddfe29a06a3402fb89ec0f52295243de2b2552
josephjohn2806 <josephsjohn2806@gmail.com>
2026-01-12 22:00:46 +0300

    Delete scripts directory

--- scripts/getdates.js
+++
@@ -1,7 +0,0 @@
-// Set current year
-const yearSpan = document.getElementById("year");
-yearSpan.textContent = new Date().getFullYear();
-
-// Set last modified date
-const lastModified = document.getElementById("lastModified");
-lastModified.textContent = document.lastModified;
