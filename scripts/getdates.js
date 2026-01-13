06d6a63045d2d7b0ea52a412a6a1c119b56e4a27
joseph john <wamokolobetse.john@gmail.com>
2026-01-13 01:16:02 -0800

    add index.html , styles and scripts

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,7 +1,7 @@
-// Set current year
-const yearSpan = document.getElementById("year");
-yearSpan.textContent = new Date().getFullYear();
+// Set the current year in the footer
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
 
-// Set last modified date
-const lastModified = document.getElementById("lastModified");
-lastModified.textContent = document.lastModified;
+// Set the last modified date in the footer
+document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
+
